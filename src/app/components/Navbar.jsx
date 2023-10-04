4cdec0ce595dbd8db1e8497579ffe81b57eff6b5
viewprox86 <workmates.live@outlook.com>
2023-10-04 15:14:20 -0500

    adsdas

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -8,15 +8,15 @@ import MenuOverlay from "./MenuOverlay";
 const navLinks = [
   {
     title: "About",
-    path: "#about",
+    path: "# about",
   },
   {
     title: "Projects",
-    path: "#projects",
+    path: "# projects",
   },
   {
     title: "Contact",
-    path: "#contact",
+    path: "# contact",
   },
 ];
 
