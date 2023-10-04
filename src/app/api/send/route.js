d4f0339858fae53daadfcf9b862742b82c3f706e
viewprox86 <workmates.live@outlook.com>
2023-10-04 16:07:05 -0500

    as

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,20 +1,16 @@
-// import { EmailTemplate } from '../../../components/EmailTemplate';
-import { NextResponse } from "next/server";
-import { Resend } from "resend";
+import { EmailTemplate } from '../../../components/EmailTemplate';
+import { NextResponse } from 'next/server';
+import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST() {
   try {
     const data = await resend.emails.send({
-      from: "Dojos <support@workdojos.com>",
-      to: ["support@workdojos.com"],
-      subject: "Thanks!",
-      react: (
-        <>
-          <p>Got it!  Thanks!</p>
-        </>
-      ),
+      from: 'Dojos <support@workdojos.com>',
+      to: ['support@workdojos.com'],
+      subject: 'Thanks!',
+    ##  react: EmailTemplate({ firstName: 'John' }),
     });
 
     return NextResponse.json(data);
@@ -22,3 +18,4 @@ export async function POST() {
     return NextResponse.json({ error });
   }
 }
+
