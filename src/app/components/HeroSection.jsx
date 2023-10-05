b582d867116fa54bf81347d5794696fc7719b330
viewprox86 <workmates.live@outlook.com>
2023-10-05 09:02:00 -0500

    sadfsd

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -17,18 +17,22 @@ const HeroSection = () => {
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
-              Hello, I&apos;m{" "}
+              Hello,{" "}
             </span>
             <br></br>
             <TypeAnimation
               sequence={[
-                "Judy",
+                "I'm Jay",
                 1000,
-                "Web Developer",
+                "Digital Marketer",
                 1000,
-                "Mobile Developer",
+                "Project Manager",
+                1000,
+                "SysAdmin",
                 1000,
                 "UI/UX Designer",
+                1000,
+                "Entrepreneur",
                 1000,
               ]}
               wrapper="span"
@@ -37,22 +41,21 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-            voluptatum.
+            I'm a big idea guy with the tech chops to back it up.
           </p>
           <div>
             <Link
-              href="/contact"
+              href="https://meet.admins.live/join?room=Kevin+Bacon&name=Boss"
               className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
             >
-              Hire Me
+              Call me
             </Link>
             <Link
-              href="/"
+              href="mailto:jay@admins.live"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-                Download CV
+                E-mail me
               </span>
             </Link>
           </div>
@@ -65,7 +68,7 @@ const HeroSection = () => {
         >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/images/hero-image.png"
+              src="/images/me argentina backpacker token.png"
               alt="hero image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
