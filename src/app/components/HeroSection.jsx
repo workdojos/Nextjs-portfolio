b4e5552b0c35c5acf20a40012b7f9bdf3a7c235a
viewprox86 <workmates.live@outlook.com>
2023-12-20 13:54:29 -0600

    df

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -39,7 +39,7 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            I&apos;m a big idea guy with self-taught tech chops.
+            I&apos;m a big idea guy and a self-taught developer.
           </p>
           <div>
             <Link
