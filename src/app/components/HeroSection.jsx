3cf3e7d7006e4f1b010dbcdeefed74b2b51b4c26
viewprox86 <workmates.live@outlook.com>
2024-02-13 14:25:36 -0600

    as

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -43,10 +43,10 @@ const HeroSection = () => {
           </p>
           <div>
             <Link
-              href="https://meet.admins.live/join?room=Kevin+Bacon&name=Guest"
+              href="https://calendly.com/dojos"
               className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
             >
-              Call me
+              Calendar
             </Link>
             <Link
               href="mailto:jay@admins.live"
